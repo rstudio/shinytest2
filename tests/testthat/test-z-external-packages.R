17497a0c8fad3eca11cce1c2fff80096edd5fe70
Barret Schloerke <schloerke@gmail.com>
2021-10-07 14:46:13 -0400

    Use `expect_snapshot_app` methods

--- tests/testthat/test-z-external-packages.R
+++ tests/testthat/test-z-external-packages.R
@@ -5,5 +5,5 @@ test_that("plotly webgl works", {
 
   app <- ShinyDriver2$new(test_path("apps/plotly-webgl"), variant = os_name_and_r_version());
   app$waitForValue("p", iotype = "output", ignore = list(NULL))
-  app$expectSnapshot(items = list(output = "p"), screenshot = TRUE)
+  expect_snapshot_app(app, items = list(output = "p"), screenshot = TRUE)
 })
