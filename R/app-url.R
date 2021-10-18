67f7b04a53c24036f1b2e8113d4c3a9b8e16da0c
Barret Schloerke <barret@rstudio.com>
2021-10-18 15:11:52 -0400

    Remove unnecessary url parts (#26)

--- R/app-url.R
+++ R/app-url.R
@@ -1,19 +1,10 @@
 #' @include shiny-driver.R
-# TODO-barret; Why do these values exist when we have `public$getUrl()`? They should all be removed in favor of a single `$shinyURL` field
-# https://github.com/rstudio/shinytest2/issues/22
-ShinyDriver2$set("private", "shinyUrlProtocol", NULL) # "http" or "https"
-ShinyDriver2$set("private", "shinyUrlHost",     NULL) # usually 127.0.0.1
-ShinyDriver2$set("private", "shinyUrlPort",     NULL)
-ShinyDriver2$set("private", "shinyUrlPath",     NULL)
+ShinyDriver2$set("private", "shinyUrl", NULL)
 
 
 #' @include shiny-driver.R
 ShinyDriver2$set("private", "getShinyUrl", function() {
-  paste0(
-    private$shinyUrlProtocol, "://", private$shinyUrlHost,
-    if (!is.null(private$shinyUrlPort)) paste0(":", private$shinyUrlPort),
-    private$shinyUrlPath
-  )
+  private$shinyUrl
 })
 
 #' @include shiny-driver.R
@@ -32,12 +23,11 @@ ShinyDriver2$set("private", "setShinyUrl", function(url) {
   ckm8_assert_single_string(res$host)
   ckm8_assert_single_url(res$path)
 
-  # nolint start
-  private$shinyUrlProtocol <- res$protocol
-  private$shinyUrlHost     <- res$host
-  private$shinyUrlPort     <- res$port
-  private$shinyUrlPath     <- res$path
-  # nolint end
+  private$shinyUrl <- paste0( # nolint
+    res$protocol, "://", res$host,
+    if (!is.null(res$port)) paste0(":", res$port),
+    res$path
+  )
 
   invisible(self)
 })
