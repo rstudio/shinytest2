448008fe77c62bea3bda468cee6673e58073536f
Barret Schloerke <schloerke@gmail.com>
2021-10-21 14:59:11 -0400

    run download test with no variant

--- tests/testthat/test-app-download.R
+++ tests/testthat/test-app-download.R
@@ -1,11 +1,7 @@
 
 test_that("Exported values", {
-  app <- ShinyDriver2$new(test_path("apps/download/"))
-
+  app <- ShinyDriver2$new(test_path("apps/download/"), variant = NULL)
   app$waitForShiny()
-
-  # app_expect_appshot(app)
-  # app$waitForValue("download_link", iotype = "output", ignore = list("", NULL))
 
   app_expect_download(app, "download_link")
   app_expect_download(app, "download_button")
