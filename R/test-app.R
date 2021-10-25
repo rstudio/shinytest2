7c64b9cd941b4ab18b3827fae68c93d07713e1d0
Barret Schloerke <schloerke@gmail.com>
2021-10-25 16:46:32 -0400

    Try to use shorter file paths

--- R/test-app.R
+++ R/test-app.R
@@ -6,14 +6,13 @@ NULL
 #'
 #' To be used within the apps `./tests` folder
 #'
-#' @inheritParams testthat::test_dir
-#' @param `./testthat` folder to execute tests over
+#' @param app_dir The base directory for the Shiny application
 #' @param env Use the shiny application's environment after sourcing the R folder
-# TODO-barret; Can this be removed and check the output of the result instead?
-#' @param reporter Use the default `testthat` reporter, but also include a `fail` reporter to assert there are no errors found
+#' @param reporter Uses the [`testthat::default_reporter()`] reporter but also includes a `fail` reporter to assert there are no errors found while testing
+#' @param ... Parameters passed to [`testthat::test_dir()`]
 #' @export
-# TODO-barret; Name ok? `test_app()` Seems too close to "test this app" vs "calling testthat on app"
 # Inspiration from https://github.com/rstudio/shiny/blob/a8c14dab9623c984a66fcd4824d8d448afb151e7/inst/app_template/tests/testthat.R
+# TODO-barret; Name ok? `test_app()` Seems too close to "test this app" vs "calling testthat on app"
 testthat_app <- function(
   app_dir = "../",
   # Run in the app's environment containing all support methods.
@@ -21,6 +20,8 @@ testthat_app <- function(
   reporter = default_reporter(),
   ...
 ) {
+  reporter <- reporter %||% "Progress"
+  stopifnot(is.character(reporter))
   # force variables before testing starts / paths change
   list2(app_dir, env, reporter, ...)
 
@@ -31,6 +32,8 @@ testthat_app <- function(
       path = file.path(app_dir, "tests", "testthat"),
       env = env,
       # Display the regular progress output and throw an error if any test error is found
+      # TODO-barret; Can this `Fail` logic be removed and check the output of the result instead?
+      #   `testthat` does not throw an error if tests fail to execute
       reporter = c(reporter, "Fail"),
       ...
     )
