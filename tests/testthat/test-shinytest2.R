b189ed47f6f22794e41fae3baa4330dd12bb1846
Barret Schloerke <schloerke@gmail.com>
2021-12-07 12:08:43 -0500

    lint

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -2,6 +2,6 @@
 # If testthat is not a `Depends` package, then `testthat` is guarenteed to be loaded when shinytest2 is loaded.
 # This will cause confusion when only `library(shinytest2)` is called.
 test_that("testthat is a depends package", {
-  depends <- as.list(read.dcf(system.file("DESCRIPTION", package = "shinytest2"))[1,])$Depends
+  depends <- as.list(read.dcf(system.file("DESCRIPTION", package = "shinytest2"))[1, ])$Depends
   expect_match(depends, "testthat")
 })
