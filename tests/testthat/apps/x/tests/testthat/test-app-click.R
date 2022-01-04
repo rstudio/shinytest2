24c96309aa16d3c833a374faab218b89de0f9227
Barret Schloerke <schloerke@gmail.com>
2022-01-04 11:18:35 -0500

    test click api on `output` and `selector`

--- tests/testthat/apps/x/tests/testthat/test-app-click.R
+++ tests/testthat/apps/x/tests/testthat/test-app-click.R
@@ -6,4 +6,37 @@ test_that("basic website example works", {
   app$set_inputs(name = "Barret")
   app$click("greet")
   app$expect_appshot(items = list(output = "greeting")) # Barret
+
+
+  # Track clicks on `#greeting` and `#custom_div`
+  app$execute_script("
+    window.greeting_count = 0;
+
+    $('#greeting').click(function () {
+      window.greeting_count += 1;
+    });
+  ")
+  app$execute_script("
+    window.custom_count = 0;
+
+    $('#custom_div').click(function () {
+      window.custom_count += 1;
+    });
+  ")
+
+  expect_equal(app$execute_script("return window.greeting_count"), 0)
+  expect_equal(app$execute_script("return window.custom_count"), 0)
+
+  app$click(output = "greeting")
+  app$click(output = "greeting")
+
+  expect_equal(app$execute_script("return window.greeting_count"), 2)
+  expect_equal(app$execute_script("return window.custom_count"), 0)
+
+  app$click(selector = "#custom_div")
+  app$click(selector = "#custom_div")
+  app$click(selector = "#custom_div")
+
+  expect_equal(app$execute_script("return window.greeting_count"), 2)
+  expect_equal(app$execute_script("return window.custom_count"), 3)
 })
