4f6ee91560bb075da084721ec2e309b63d9fad8f
Barret Schloerke <schloerke@gmail.com>
2022-01-05 11:13:03 -0500

    Shuffle code around to be more consistently located

--- R/app-driver-expect-script.R
+++ R/app-driver-expect-script.R
@@ -1,3 +1,27 @@
+app_execute_script <- function(
+  self, private,
+  script,
+  arguments = list(),
+  ...,
+  timeout = 15 * 1000
+) {
+  ckm8_assert_app_driver(self, private)
+  ellipsis::check_dots_empty()
+
+  # TODO-barret; incorporate `wait_` parameters to not wait for the _tick_ to finish?
+  # TODO-barret; Document how they should make a promise and return NULL instead?
+  "!DEBUG app_execute_script()"
+  chromote_execute_script(
+    self$get_chromote_session(),
+    script,
+    awaitPromise = TRUE,
+    arguments = arguments,
+    timeout = timeout
+  )$result$value
+}
+
+
+
 app_expect_script <- function(
   self, private,
   script,
