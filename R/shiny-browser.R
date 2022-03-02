d6b2cc2b6a5b92088cc60e6e1fd184124b3f515a
Barret Schloerke <schloerke@gmail.com>
2022-03-02 15:58:09 -0500

    Only use IDE logic if on server. Otherwise use local chrome app

--- R/shiny-browser.R
+++ R/shiny-browser.R
@@ -1,17 +1,7 @@
 with_external_shiny_browser <- function(code) {
-  browser <- chromote::default_chromote_object()$get_browser()
-  if (inherits(browser, "Chrome")) {
-    # If Chrome is locally available, use the local Chrome browser
-    browser_path <- shQuote(browser$get_path())
-    open_chrome <- function(url) {
-      browseURL(url, browser_path)
-    }
-    old_options <- options(shiny.launch.browser = open_chrome)
-    withr::defer({
-      options(old_options)
-    })
+  if (rstudio_is_available() && rstudioapi::versionInfo()$mode == "server") {
+    # Server!
 
-  } else if (rstudio_is_available()) {
     # If in the IDE, force the url to open in the external browser
     # Inspriation: https://github.com/rstudio/shinycoreci/blob/fa634714f14df8130cb60a6a1c8f07f53603a27b/R/test_in_ide.R#L98-L145
     if (rstudioapi::isAvailable("1.3.387")) {
@@ -31,6 +21,22 @@ with_external_shiny_browser <- function(code) {
       old_option <- options(shiny.launch.browser = open_external)
       withr::defer({
         options(old_option)
+      })
+
+    }
+  } else {
+    # Local! (or Not RStudio!)
+    # Do not mess with the IDE options, instead just overwrite the shiny.launch.browser option
+    browser <- chromote::default_chromote_object()$get_browser()
+    if (inherits(browser, "Chrome")) {
+      # If Chrome is locally available, use the local Chrome browser
+      browser_path <- shQuote(browser$get_path())
+      open_chrome <- function(url) {
+        browseURL(url, browser_path)
+      }
+      old_options <- options(shiny.launch.browser = open_chrome)
+      withr::defer({
+        options(old_options)
       })
 
     }
