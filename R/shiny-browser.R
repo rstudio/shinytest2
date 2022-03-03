2dcacee47e050f6d438effbffb495b6f8d146de8
Barret Schloerke <schloerke@gmail.com>
2022-03-03 10:01:02 -0500

    Rearrange logic again to prefer chrome if not in server mode

--- R/shiny-browser.R
+++ R/shiny-browser.R
@@ -1,5 +1,28 @@
 with_external_shiny_browser <- function(code) {
-  if (rstudio_is_available() && rstudioapi::versionInfo()$mode == "server") {
+  ## Logic
+  # if Chrome and not RStudio server, open with chrome
+  # else if RStudio is available, set RStudio prefs
+  # else (not Chrome and no RStudio), do nothing
+
+  is_rstudio_server <- rstudio_is_available() && rstudioapi::versionInfo()$mode == "server"
+
+  # Match the same Chromote object when initializing AppDriver's Chromote session
+  browser <- chromote::default_chromote_object()$get_browser()
+  if ((!is_rstudio_server) && inherits(browser, "Chrome")) {
+    # Local! (or not RStudio server!)
+    # Do not mess with the IDE options, instead just overwrite the
+    # shiny.launch.browser option
+
+    # If Chrome is locally available, use the local Chrome browser
+    browser_path <- shQuote(browser$get_path())
+    open_chrome <- function(url) {
+      browseURL(url, browser_path)
+    }
+    old_options <- options(shiny.launch.browser = open_chrome)
+    withr::defer({
+      options(old_options)
+    })
+  } else if (rstudio_is_available()) {
     # Server!
 
     # If in the IDE, force the url to open in the external browser
@@ -21,22 +44,6 @@ with_external_shiny_browser <- function(code) {
       old_option <- options(shiny.launch.browser = open_external)
       withr::defer({
         options(old_option)
-      })
-
-    }
-  } else {
-    # Local! (or Not RStudio!)
-    # Do not mess with the IDE options, instead just overwrite the shiny.launch.browser option
-    browser <- chromote::default_chromote_object()$get_browser()
-    if (inherits(browser, "Chrome")) {
-      # If Chrome is locally available, use the local Chrome browser
-      browser_path <- shQuote(browser$get_path())
-      open_chrome <- function(url) {
-        browseURL(url, browser_path)
-      }
-      old_options <- options(shiny.launch.browser = open_chrome)
-      withr::defer({
-        options(old_options)
       })
 
     }
