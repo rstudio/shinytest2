rstudio/shinytest2

commit eec4983d19109254055bd5ccae911d89320a7f50

Only use IDE viewer logic if on `server`. Otherwise use local chrome app (#120)
* Only use IDE logic if on server. Otherwise use local chrome app

* Rearrange logic again to prefer chrome if not in server mode

* Adjust logic again after writing out use cases

* Add comment

* Namespace utils methods

R/shiny-browser.R
@@ -1,38 +1,63 @@
 with_external_shiny_browser <- function(code) {
-  browser <- chromote::default_chromote_object()$get_browser()
-  if (inherits(browser, "Chrome")) {
-    # If Chrome is locally available, use the local Chrome browser
-    browser_path <- shQuote(browser$get_path())
-    open_chrome <- function(url) {
-      browseURL(url, browser_path)
-    }
-    old_options <- options(shiny.launch.browser = open_chrome)
-    withr::defer({
-      options(old_options)
-    })
+  ## Logic
 
-  } else if (rstudio_is_available()) {
-    # If in the IDE, force the url to open in the external browser
-    # Inspriation: https://github.com/rstudio/shinycoreci/blob/fa634714f14df8130cb60a6a1c8f07f53603a27b/R/test_in_ide.R#L98-L145
-    if (rstudioapi::isAvailable("1.3.387")) {
-      # See https://docs.rstudio.com/ide/server-pro/1.3.1073-1/session-user-settings.html
-      # user, none, pane, window, browser; Default is 'window'
-      shiny_viewer_type <- rstudioapi::readRStudioPreference("shiny_viewer_type", "window")
-      # Restore option
+  ## If Chrome is available, open in Chrome, except if RStudio server.
+  ## RStudio desktop can not open in: window, pane. Only external
+  ## RStudio server can open in any viewer location: window, pane, external.
+  ##   Do not adjust settings
+
+  ## Therefore,
+  ## If not RStudio server
+  ##   If Chrome, open in Chrome
+  ##   else If RStudio desktop, open in external
+
+  is_avail <- rstudio_is_available()
+  rstudio_mode <- rstudioapi::versionInfo()$mode
+  is_rstudio_server <- is_avail && rstudio_mode == "server"
+  is_rstudio_desktop <- is_avail && rstudio_mode != "server"
+
+  if (!is_rstudio_server) {
+    # Match the same Chromote object when initializing AppDriver's Chromote session
+    # Use similar logic to `browse_url()`
+    browser <- chromote::default_chromote_object()$get_browser()
+    if (inherits(browser, "Chrome")) {
+      # Has local Chrome browser!
+      # Do not mess with the IDE options, instead just overwrite the
+      # shiny.launch.browser option
+
+      # Use the local Chrome browser path and shell escape it
+      browser_path <- shQuote(browser$get_path())
+      open_chrome <- function(url) {
+        utils::browseURL(url, browser_path)
+      }
+      old_options <- options(shiny.launch.browser = open_chrome)
       withr::defer({
-        rstudioapi::writeRStudioPreference("shiny_viewer_type", shiny_viewer_type)
+        options(old_options)
       })
+    } else if (is_rstudio_desktop) {
+      # Local RStudio desktop!
 
       # Force the url to open in the external browser
-      rstudioapi::writeRStudioPreference("shiny_viewer_type", "browser")
-    } else {
-      # RStudio, but early version
-      open_external <- get(".rs.invokeShinyWindowExternal", envir = as.environment("tools:rstudio"))
-      old_option <- options(shiny.launch.browser = open_external)
-      withr::defer({
-        options(old_option)
-      })
+      # Inspriation: https://github.com/rstudio/shinycoreci/blob/fa634714f14df8130cb60a6a1c8f07f53603a27b/R/test_in_ide.R#L98-L145
+      if (rstudioapi::isAvailable("1.3.387")) {
+        # See https://docs.rstudio.com/ide/server-pro/1.3.1073-1/session-user-settings.html
+        # user, none, pane, window, browser; Default is 'window'
+        shiny_viewer_type <- rstudioapi::readRStudioPreference("shiny_viewer_type", "window")
+        # Restore option
+        withr::defer({
+          rstudioapi::writeRStudioPreference("shiny_viewer_type", shiny_viewer_type)
+        })
 
+        # Force the url to open in the external browser
+        rstudioapi::writeRStudioPreference("shiny_viewer_type", "browser")
+      } else {
+        # RStudio, but early version
+        open_external <- get(".rs.invokeShinyWindowExternal", envir = as.environment("tools:rstudio"))
+        old_option <- options(shiny.launch.browser = open_external)
+        withr::defer({
+          options(old_option)
+        })
+      }
     }
   }
 

R/use.R
@@ -62,7 +62,7 @@ use_shinytest2_package <- function(app_dir = ".", quiet = FALSE) {
     with_this_project({
       wrapper <-
         if (quiet) function(...) {
-          capture.output(..., type = "message")
+          utils::capture.output(..., type = "message")
         } else {
           force
         }

tests/testthat/test-shinytest2.R
@@ -10,7 +10,7 @@ test_that("testthat is a depends package", {
 
 test_that("AppDriver can print while working with `missing_arg()` values", {
   expect_error(
-    capture.output({
+    utils::capture.output({
       print(AppDriver$new(test_path("apps/x")))
     }),
     NA

commit e94405fbfac785ffd120b5ed0526684bde83af18

Use `{shinyvalidate}` to validate recorder inputs (#123)
* Rename `./R/record_test.R` -> `./R/record-test.R`

* More file renames

* Move `expr_recurse` to its own file

* Fix bug introduced in #120

* If a recorder name is provided, make sure it unique. Use shinyvalidate

* Do not allow symbols. Add test coverage

* Remove version number for now

* Fix installation issues

DESCRIPTION
@@ -41,13 +41,15 @@ Suggests:
     rstudioapi,
     shinyWidgets,
     shinytest (>= 1.5.1),
+    shinyvalidate,
     showimage,
     knitr,
     usethis
 Remotes:
     rstudio/chromote
 Config/Needs/other:
-    shiny (>= 1.7.1.9003)
+    shiny (>= 1.7.1.9003),
+    shinyvalidate (>= 1.0.0.9001),
 Config/Needs/website:
     pkgdown,
     tidyverse/tidytemplate
@@ -71,18 +73,20 @@ Collate:
     'app-driver-wait.R'
     'app-driver-window.R'
     'app-driver.R'
-    'chromote_methods.R'
+    'chromote-methods.R'
     'expect-snapshot.R'
+    'expr-recurse.R'
     'httr.R'
     'migrate.R'
     'missing-value.R'
     'utils.R'
     'platform.R'
-    'record_test.R'
+    'record-test-unique-name.R'
+    'record-test.R'
     'rstudio.R'
     'save-app.R'
     'shiny-browser.R'
-    'shinytest2_logs.R'
+    'shinytest2-logs.R'
     'test-app.R'
     'use.R'
 VignetteBuilder: knitr

R/expr-recurse.R
@@ -1,0 +1,43 @@
+expr_recurse <- function(expr, post_fn) {
+  expr_recurse_ <- function(expr, post_fn, is_top_level = FALSE) {
+    if (!is.language(expr)) {
+      return(expr)
+    }
+    expr_list <- as.list(expr)
+
+    if (
+      # Return early if it is a single item
+      length(expr_list) == 1 &&
+      # Make sure not something like `app$getAllValues()`
+      is.language(expr_list[[1]]) &&
+      length(expr_list[[1]]) == 1
+    ) {
+      return(expr)
+    }
+    for (i in seq_len(length(expr_list))) {
+      expr_list[[i]] <-
+        expr_recurse_(expr_list[[i]], post_fn, is_top_level = FALSE)
+    }
+
+    # By being after the for-loop, it alters from the leaf to the trunk
+    post_fn(expr_list, is_top_level)
+  }
+  # Shim `is_top_level = TRUE`
+  expr_recurse_(expr = expr, post_fn = post_fn, is_top_level = TRUE)
+}
+
+
+st2_expr_text <- function(expr) {
+  if (is.null(expr) || is.character(expr)) return(expr)
+  if (is.list(expr)) return(lapply(expr, st2_expr_text))
+  gsub(
+    "\\s*\n    ",
+    "\n  ",
+    rlang::expr_text(expr, width = 60L)
+  )
+}
+for_each_expr_text <- function(exprs, expr_fn, ...) {
+  unlist(lapply(exprs, function(expr) {
+    st2_expr_text(expr_fn(expr, ...))
+  }))
+}

R/migrate.R
@@ -109,7 +109,7 @@ m__find_shinytest_testapp <- function(exprs, info_env) {
   }
   # For all exprs, find a single shinytest::testApp()
   lapply(exprs, function(expr) {
-    m__recurse_expr(expr, post_fn = post_fn)
+    expr_recurse(expr, post_fn = post_fn)
   })
 
   args
@@ -150,7 +150,7 @@ m__find_shinydriver_new <- function(exprs, info_env) {
   }
   # For all exprs, find a single shinytest::testApp()
   lapply(exprs, function(expr) {
-    m__recurse_expr(expr, post_fn = post_fn)
+    expr_recurse(expr, post_fn = post_fn)
   })
 
   ret
@@ -464,13 +464,13 @@ m__driver_new <- function(expr, info_env) {
       !!!init_args
     )
 
-    # Signify that a match was found for m__recurse_expr
+    # Signify that a match was found for m__algo_2
     info_env$match_found <- TRUE
     # Return upgraded expr
     ret_expr
   }
 
-  m__recurse_expr(expr, post_fn = post_fn)
+  expr_recurse(expr, post_fn = post_fn)
 }
 
 # Copy over code to avoid being hosed down the road
@@ -580,59 +580,6 @@ m__algo_2 <- function(test_text, expr_fn, info_env) {
 }
 
 
-
-# m__shinytest_lang <- function(expr, info_env, is_top_level = FALSE) {
-#   shinytest_lang_is_fn <- function(expr_list) {
-#     expr_fn <- expr_list[[1]]
-
-#     is.language(expr_fn) &&
-#       length(expr_fn) >= 3 &&
-#       expr_fn[[1]] == "$" &&
-#       expr_fn[[2]] == info_env$app_var
-#   }
-
-#   if (!is.language(expr)) {
-#     return(expr)
-#   }
-#   expr_list <- as.list(expr)
-
-#   if (
-#     # Return early if it is a single item
-#     length(expr_list) == 1 &&
-#     # Make sure not something like `app$getAllValues()`
-#     is.language(expr_list[[1]]) &&
-#     length(expr_list[[1]]) == 1
-#   ) {
-#     return(expr)
-#   }
-#   # # Some methods return a list of values
-#   # new_expr_list <- list()
-#   # for (expr_list_item in expr_list) {
-#   #   new_expr_list <- append(
-#   #     new_expr_list,
-#   #     m__shinytest_lang(expr_list[[i]], info_env, is_top_level = FALSE)
-#   #   )
-#   # }
-#   for (i in seq_len(length(expr_list))) {
-#     expr_list[[i]] <-
-#       m__shinytest_lang(expr_list[[i]], info_env, is_top_level = FALSE)
-#   }
-
-#   if (!shinytest_lang_is_fn(expr_list)) {
-#     return(
-#       # Reconstruct language call
-#       rlang::call2(expr_list[[1]], !!!expr_list[-1])
-#     )
-#   }
-
-#   # By being after the for-loop, it alters from the leaf to the trunk
-#   # Mark that a match was found
-#   info_env$match_found <- TRUE
-#   # Match against known function names in expr_list[[3]]
-#   matched_expr <- match_shinytest_expr(expr_list, is_top_level, info_env)
-#   matched_expr
-# }
-
 m__shinytest_lang <- function(expr, info_env) {
   shinytest_lang_is_fn <- function(expr_list) {
     expr_fn <- expr_list[[1]]
@@ -657,37 +604,10 @@ m__shinytest_lang <- function(expr, info_env) {
     matched_expr <- match_shinytest_expr(expr_list, is_top_level, info_env)
     matched_expr
   }
-  m__recurse_expr(expr, post_fn = post_fn)
+  expr_recurse(expr, post_fn = post_fn)
 }
 
 
-m__recurse_expr <- function(expr, post_fn) {
-  m__recurse_expr_ <- function(expr, post_fn, is_top_level = FALSE) {
-    if (!is.language(expr)) {
-      return(expr)
-    }
-    expr_list <- as.list(expr)
-
-    if (
-      # Return early if it is a single item
-      length(expr_list) == 1 &&
-      # Make sure not something like `app$getAllValues()`
-      is.language(expr_list[[1]]) &&
-      length(expr_list[[1]]) == 1
-    ) {
-      return(expr)
-    }
-    for (i in seq_len(length(expr_list))) {
-      expr_list[[i]] <-
-        m__recurse_expr_(expr_list[[i]], post_fn, is_top_level = FALSE)
-    }
-
-    # By being after the for-loop, it alters from the leaf to the trunk
-    post_fn(expr_list, is_top_level)
-  }
-  # Shim `is_top_level = TRUE`
-  m__recurse_expr_(expr = expr, post_fn = post_fn, is_top_level = TRUE)
-}
 
 
 
@@ -1226,21 +1146,6 @@ match_shinytest_expr <- function(expr_list, is_top_level, info_env) {
   )
 }
 
-
-st2_expr_text <- function(expr) {
-  if (is.null(expr) || is.character(expr)) return(expr)
-  if (is.list(expr)) return(lapply(expr, st2_expr_text))
-  gsub(
-    "\\s*\n    ",
-    "\n  ",
-    rlang::expr_text(expr, width = 60L)
-  )
-}
-for_each_expr_text <- function(exprs, expr_fn, ...) {
-  unlist(lapply(exprs, function(expr) {
-    st2_expr_text(expr_fn(expr, ...))
-  }))
-}
 
 
 migrate_shinycoreci_shinytest <- function(app_path) {

R/record-test-unique-name.R
@@ -1,0 +1,45 @@
+
+known_app_driver_name_values <- function(test_file) {
+
+  exprs <- parse(file = test_file)
+
+  is_appdriver_new <- function(expr_list) {
+    length(expr_list) >= 1 &&
+      is.language(expr_list[[1]]) &&
+      st2_expr_text(expr_list[[1]]) %in% c(
+        "AppDriver$new",
+        "shinytest2::AppDriver$new",
+        "shinytest2:::AppDriver$new"
+      )
+  }
+
+  known_names <- list()
+  post_fn <- function(expr_list, is_top_level) {
+    if (is_appdriver_new(expr_list)) {
+      args <- rlang::call_args(
+        rlang::call_match(
+          as.call(expr_list),
+          AppDriver$public_methods$initialize
+        )
+      )
+
+      new_name <- args$name
+      if (
+        is.null(new_name) || # allow `NULL`
+        rlang::is_scalar_character(new_name) # allow single character value
+        # Do not allow symbols or language expressions
+      ) {
+        # Allows the collecting of NULL name values
+        known_names[length(known_names) + 1] <<- list(new_name)
+      }
+    }
+    # Don't alter the expr_list, just return it
+    as.call(expr_list)
+  }
+  # For all exprs, find a single shinytest::testApp()
+  lapply(exprs, function(expr) {
+    expr_recurse(expr, post_fn = post_fn)
+  })
+
+  known_names
+}

R/shiny-browser.R
@@ -12,7 +12,7 @@ with_external_shiny_browser <- function(code) {
   ##   else If RStudio desktop, open in external
 
   is_avail <- rstudio_is_available()
-  rstudio_mode <- rstudioapi::versionInfo()$mode
+  rstudio_mode <- is_avail && rstudioapi::versionInfo()$mode
   is_rstudio_server <- is_avail && rstudio_mode == "server"
   is_rstudio_desktop <- is_avail && rstudio_mode != "server"
 

inst/internal/recorder/app.R
@@ -256,7 +256,7 @@ generate_test_code <- function(events, name, seed) {
         # Going with "no" for now as it is difficult to capture the expression
         # when nothing else is an expression
         if (has_expect_screenshot) "variant = platform_variant()",
-        if (!is.null(name)) paste0("name = ", deparse2(name)),
+        if (isTRUE(nzchar(name))) paste0("name = ", deparse2(name)),
         if (!is.null(seed)) paste0("seed = ", seed),
         if (!is.null(height)) paste0("height = ", height),
         if (!is.null(width)) paste0("width = ", width),
@@ -312,7 +312,7 @@ shinyApp(
     ),
     div(id = "shiny-recorder",
       div(class = "shiny-recorder-header", tags$code("{shinytest2}"), "expections"),
-      div(class = "shiny-recorder-controls",
+      div(class = "shiny-recorder-controls form-group",
         actionButton("values",
           span(
             img(src = "shiny.png", class = "shiny-recorder-icon", style = "height: 23px;vertical-align: middle;"),
@@ -351,6 +351,7 @@ shinyApp(
             numeric_input("seed",
               label = "Random seed:",
               value = start_seed,
+              min = 0,
               placeholder = "(None)"
             ),
             class = "inline-input-container"
@@ -411,41 +412,48 @@ shinyApp(
     trim_testevents <- reactive({
       events <- input$testevents
 
-      to_remove <- c()
-      for (i in seq_along(events)) {
-        if (i == 1) next
-        prev_event <- events[[i - 1]]
-        curr_event <- events[[i]]
-        if (prev_event$type == curr_event$type) {
-          switch(curr_event$type,
-            "outputEvent" = , # nolint
-            "waitForIdle" = , # nolint
-            "setWindowSize" = {
-              # Remove previous event
-              to_remove[length(to_remove) + 1] <- i - 1
-            },
-            "inputEvent" = {
-              if (!isTRUE(allow_no_input_binding_react())) {
-                if (!curr_event$hasBinding && !prev_event$hasBinding) {
-                  to_remove[length(to_remove) + 1] <- i
+      has_removed <- TRUE
+      # Might repeat ~ 3 times
+      while (has_removed) {
+        has_removed <- FALSE
+        to_remove <- c()
+
+        for (i in seq_along(events)) {
+          if (i == 1) next
+          prev_event <- events[[i - 1]]
+          curr_event <- events[[i]]
+          if (prev_event$type == curr_event$type) {
+            switch(curr_event$type,
+              "outputEvent" = , # nolint
+              "waitForIdle" = , # nolint
+              "setWindowSize" = {
+                # Remove previous event
+                to_remove[length(to_remove) + 1] <- i - 1
+              },
+              "inputEvent" = {
+                if (!isTRUE(allow_no_input_binding_react())) {
+                  if (!curr_event$hasBinding && !prev_event$hasBinding) {
+                    to_remove[length(to_remove) + 1] <- i
+                  }
                 }
               }
-            }
-          )
-        } else if (
-          i >= 3 &&
-          curr_event$type == "setWindowSize" &&
-          prev_event$type == "outputEvent" &&
-          events[[i - 2]]$type == "setWindowSize"
-        ) {
-          # If two setWindowSize events sandwich an outputEvent,
-          # remove the first setWindowSize
-          to_remove[length(to_remove) + 1] <- i - 2
+            )
+          } else if (
+            i >= 3 &&
+            curr_event$type == "setWindowSize" &&
+            prev_event$type == "outputEvent" &&
+            events[[i - 2]]$type == "setWindowSize"
+          ) {
+            # If two setWindowSize events sandwich an outputEvent,
+            # remove the first setWindowSize
+            to_remove[length(to_remove) + 1] <- i - 2
+          }
         }
-      }
 
-      if (length(to_remove)) {
-        events <- events[-to_remove]
+        if (length(to_remove)) {
+          has_removed <- TRUE
+          events <- events[-to_remove]
+        }
       }
 
       found_first_set_window_size <- FALSE
@@ -540,24 +548,53 @@ shinyApp(
       events
     })
 
-    # Use reactiveVal dedupe feature
-    save_enabled <- reactiveVal(FALSE)
-    save_enable_obs <- observeEvent(trim_testevents(), {
+    has_expectation_event <- reactive({
       for (event in trim_testevents()) {
         switch(event$type,
           "expectValues" = , # nolint
           "expectScreenshot" = , # nolint
           "expectDownload" = {
-            save_enabled(TRUE)
-            session$sendCustomMessage(
-              "enable_save_button",
-              "1"
-            )
-            save_enable_obs$destroy()
-            return()
+            return(TRUE)
           }
         )
       }
+      FALSE
+    })
+
+    testname_validator <- function(name) {
+      if (is.null(name)) return()
+      if (!fs::file_exists(test_save_file)) return()
+
+      cur_test_names <- known_app_driver_name_values(test_save_file)
+      # Convert names to chars
+      cur_test_names <- unique(as.character(lapply(cur_test_names, function(x) {
+        x %||% "`NULL`"
+      })))
+      if (name %in% cur_test_names) {
+        shiny::tags$div(
+          "Please use a unique name. Known names:",
+          shiny::tags$ul(
+            lapply(cur_test_names, shiny::tags$li)
+          )
+        )
+      }
+    }
+    iv <- shinyvalidate::InputValidator$new()
+    iv$add_rule("testname", testname_validator)
+    iv$add_rule("seed", shinyvalidate::sv_integer(allow_na = TRUE))
+    iv_screenshot <- shinyvalidate::InputValidator$new()
+    iv_screenshot$condition(~ !has_expectation_event())
+    iv_screenshot$add_rule("screenshot", ~ "At least one expectation must be made")
+
+    iv$add_validator(iv_screenshot)
+    iv$enable()
+
+    # Use reactiveVal dedupe feature
+    save_enabled <- reactiveVal(FALSE)
+    save_enable_obs <- observe({
+      enable_save <- iv$is_valid()
+      save_enabled(enable_save)
+      session$sendCustomMessage("enable_save_button", enable_save)
     })
 
     # If an unbound input value is updated, ask the user if the event should be recorded
@@ -646,7 +683,6 @@ shinyApp(
         ))
       )
     })
-
 
     observeEvent(input$exit_save, {
       req(save_enabled())

inst/internal/recorder/www/inject-recorder.js
@@ -147,7 +147,7 @@ window.recorder = (function() {
 
         // Enable save button when there is an expectation
         Shiny.addCustomMessageHandler("enable_save_button", function(message) {
-            $("#exit_save").removeClass("disabled");
+            $("#exit_save").toggleClass("disabled", !message);
         });
 
     });

inst/internal/recorder/www/recorder.css
@@ -95,7 +95,14 @@ html,body {
 /* #shiny-recorder .shiny-recorder-controls .inline-input-container {
   display: inline-block;
 } */
-#shiny-recorder #save-and-quit .inline-input-container * {
+#shiny-recorder .shiny-recorder-controls .help-block {
+  margin-bottom: 5px;
+}
+#shiny-recorder .shiny-recorder-controls.form-group {
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+#shiny-recorder #save-and-quit .inline-input-container *:not(.shiny-validation-message *){
   display: inline-block;
 }
 #shiny-recorder #save-and-quit .inline-input-container input[type="text"] {

man/register_input_processor.Rd
@@ -1,5 +1,5 @@
 % Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/record_test.R
+% Please edit documentation in R/record-test.R
 \name{register_input_processor}
 \alias{register_input_processor}
 \alias{get_input_processors}

man/record_test.Rd
@@ -1,5 +1,5 @@
 % Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/record_test.R
+% Please edit documentation in R/record-test.R
 \name{record_test}
 \alias{record_test}
 \title{Launch test event recorder for a Shiny app}

tests/testthat/test-known-names.R
@@ -1,0 +1,22 @@
+
+test_that("known names are being found", {
+  tmpfile <- tempfile()
+  withr::defer({
+    if (fs::file_exists(tmpfile)) {
+      fs::file_delete(tmpfile)
+    }
+  })
+  cat(file = tmpfile, "
+  AppDriver$new()
+  AppDriver$new(name = NULL)
+  shinytest2::AppDriver$new(name = 'test')
+  shinytest2:::AppDriver$new(name = 'test2')
+  some_value <- 'value'
+  AppDriver$new(name = some_value)
+  ")
+
+  expect_equal(
+    known_app_driver_name_values(tmpfile),
+    list(NULL, NULL, "test", "test2")
+  )
+})