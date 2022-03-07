a79217298c909b5b56eb04937cb4bbe647011d44
Barret Schloerke <schloerke@gmail.com>
2022-03-07 16:12:29 -0500

    lint

--- tests/testthat/apps/update/app.R
+++ tests/testthat/apps/update/app.R
@@ -19,7 +19,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 
   observe({
-    updateSliderInput(inputId = "slider", value = input$click)
+    shiny::updateSliderInput(inputId = "slider", value = input$click)
     session$sendCustomMessage("count", input$click)
   })
 
