54f76d98b3cad92a0ed369428ad8bf1eadd93312
Barret Schloerke <schloerke@gmail.com>
2022-03-08 15:54:02 -0500

    Only test fixed outputs

--- tests/testthat/apps/rmd-shiny/tests/testthat/test-rmd.R
+++ tests/testthat/apps/rmd-shiny/tests/testthat/test-rmd.R
@@ -4,7 +4,7 @@ test_that("Shiny R Markdown documents can test", {
   app <- AppDriver$new(seed = 9767)
   app$set_inputs(name = "barret")
   app$click("greet")
-  app$expect_values()
+  app$expect_values(input = TRUE, output = "greeting")
   expect_equal(
     app$get_value(output = "greeting"),
     "Hello barret!"
