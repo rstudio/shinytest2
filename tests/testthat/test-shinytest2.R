50a0df26f0a5b6ca1609fe02499d6600a30dd9dc
Barret Schloerke <schloerke@gmail.com>
2022-03-08 10:19:48 -0500

    Update path location

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -11,7 +11,7 @@ test_that("testthat is a depends package", {
 test_that("AppDriver can print while working with `missing_arg()` values", {
   expect_error(
     utils::capture.output({
-      print(AppDriver$new(test_path("apps/x")))
+      print(AppDriver$new(test_path("apps/hello")))
     }),
     NA
   )
