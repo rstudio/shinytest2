77834f07a57d9f27d62d58918107a09ff30ca945
Barret Schloerke <schloerke@gmail.com>
2022-03-10 14:27:09 -0500

    Update package-install.R

--- .github/shiny-workflows/package-install.R
+++ .github/shiny-workflows/package-install.R
@@ -1,2 +1,6 @@
+if (!require("pak")) {
+  options(pak.no_extra_messages = TRUE)
+  install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/")
+}
 pak::pkg_install("rstudio/shiny")
 pak::pkg_install("rstudio/shinyvalidate")
