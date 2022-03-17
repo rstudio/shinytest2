f606056b707adf955e6f9a63235e8aadfa49da4e
Barret Schloerke <schloerke@gmail.com>
2022-03-17 11:49:36 -0400

    If content disposition filename is provided, save as snapshot value

--- R/app-driver-expect-download.R
+++ R/app-driver-expect-download.R
@@ -29,8 +29,19 @@ app_download <- function(
   download_path <- fs::path(private$save_dir, name)
   writeBin(req$content, download_path)
 
+  content_dispo <- httr::headers(req)[["content-disposition"]]
+  filename_header <- NULL
+  if (length(content_dispo) == 1 && is.character(content_dispo) && nzchar(content_dispo)) {
+    vals <- strsplit(content_dispo, "filename=")[[1]]
+    if (length(vals) > 1) {
+      filename_header <- gsub("\"", "", vals[2], fixed = TRUE)
+      self$log_message(paste0("Content disposition file name: ", filename_header))
+    }
+  }
+
   list(
-    download_path = download_path
+    download_path = download_path,
+    filename_header = filename_header
   )
 }
 
@@ -48,13 +59,23 @@ app_expect_download <- function(
 
   snapshot_info <- app_download(self, private, id = id, name = name)
 
-  # compare download_file
+  # Compare download_file content
   app__expect_snapshot_file(
     self, private,
     snapshot_info$download_path,
     cran = cran,
     compare = testthat::compare_file_text
   )
+
+  # Compare requested filename
+  requested_download_name <- snapshot_info$filename_header
+  if (!is.null(requested_download_name)) {
+    app__expect_snapshot_value(
+      self, private,
+      requested_download_name,
+      cran = cran
+    )
+  }
 
   invisible(self)
 }
@@ -68,11 +89,21 @@ app_get_download <- function(
   ckm8_assert_app_driver(self, private)
   # ellipsis::check_dots_empty()
 
-  filename <- filename %||% tempfile(fileext = ".download")
+  snapshot_info <- app_download(self, private, id = id, name = fs::path_file(tempfile()))
+
+  filename <-
+    if (!is.null(filename))
+      filename
+    else if (!is.null(snapshot_info$filename_header))
+      # Don't allow weird file paths from the application
+      fs::path(tempfile(), fs::path_sanitize(snapshot_info$filename_header, "_"))
+    else
+      tempfile(fileext = ".download")
   ckm8_assert_single_string(filename)
 
-  snapshot_info <- app_download(self, private, id = id, name = fs::path_file(tempfile()))
+  fs::dir_create(fs::path_dir(filename), recurse = TRUE)
   fs::file_copy(snapshot_info$download_path, filename, overwrite = TRUE)
+  fs::file_delete(snapshot_info$download_path)
 
   filename
 }
