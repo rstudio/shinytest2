af1651df76aa78b226b15cce2b1e4442975e7023
Barret Schloerke <schloerke@gmail.com>
2022-03-28 16:17:06 -0400

    Add vig app test

---
+++ tests/testthat/apps/robust-ex/app.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+  numericInput("n", "Number of rows", 10, 1, nrow(cars)),
+  plotOutput("plot")
+)
+server <- function(input, output) {
+  dt <- reactive({
+    head(cars, input$n)
+  })
+  plot_obj <- reactive({
+    ggplot(dt(), aes_string("speed", "dist")) + geom_point()
+  })
+
+  output$plot <- renderPlot({
+    plot_obj()
+  })
+
+  exportTestValues(
+    dt = dt(),
+    plot_obj = plot_obj()
+  )
+}
+
+shinyApp(ui = ui, server = server)
