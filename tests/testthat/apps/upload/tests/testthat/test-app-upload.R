fa3595a892409fe9a73939c574e4848b9ea74d30
Barret Schloerke <schloerke@gmail.com>
2022-04-04 16:45:34 -0400

    Better error messages when uploading files
    Fixes #160

--- tests/testthat/apps/upload/tests/testthat/test-app-upload.R
+++ tests/testthat/apps/upload/tests/testthat/test-app-upload.R
@@ -6,3 +6,13 @@ test_that("Make sure upload can take use a local file", {
   app$upload_file(file = "cars.csv")
   app$expect_values()
 })
+
+test_that("Make sure upload can take use a local file", {
+  app <- AppDriver$new(name = "upload")
+
+  expect_error(
+    app$upload_file(file = "missing_file.csv"),
+    "Missing upload file at path", fixed = TRUE
+  )
+
+})
