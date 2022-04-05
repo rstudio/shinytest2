d400ccff6190d3102aa4cc938baf5d41578b5992
Barret Schloerke <schloerke@gmail.com>
2022-04-05 14:54:14 -0400

    Support handling 404 status when getting a URL (#167)
    * Support handling 404 status when getting a URL
    
    * Support altering of `call` in `rlang::abort()` of `app_abort()`
    
    * Handle 404 for `$get_values()` and `$expect_values()`
    
    * Test all the things!
    
    * Increase total time for shiny to start

--- R/app-driver-message.R
+++ R/app-driver-message.R
@@ -6,7 +6,7 @@ app_inform <- function(self, private, ...) {
   ckm8_assert_app_driver(self, private)
   rlang::inform(..., app = self)
 }
-app_abort <- function(self, private, ...) {
+app_abort <- function(self, private, ..., call = rlang::caller_env()) {
   ckm8_assert_app_driver(self, private)
-  rlang::abort(..., app = self, call = rlang::caller_env())
+  rlang::abort(..., app = self, call = call)
 }
---
+++ tests/testthat/test-not-testing.R
@@ -0,0 +1,42 @@
+skip_on_cran()
+require("shiny", quietly = TRUE, character.only = TRUE)
+
+test_that("Running an app not in testing mode has 404 handled when getting values", {
+
+  app_bg <- callr::r_bg(
+    function() { shiny::runApp("./apps/hello", test.mode = FALSE) },
+    stderr = "|"
+  )
+  withr::defer({ app_bg$kill() })
+
+  # Wait until Shiny server is running
+  app_url <- NULL
+  for (i in 1:100) {
+    lines <- paste0(app_bg$read_error_lines(), collapse = "\n")
+    if (length(lines) > 0) {
+      if (grepl("http", lines, fixed = TRUE)) {
+        print(lines)
+        lines <- strsplit(lines, "\n")[[1]]
+        line <- lines[grepl("http", lines, fixed = TRUE)]
+        app_url <- tail(strsplit(line, " ")[[1]], 1)
+        break
+      }
+    }
+    Sys.sleep(0.1)
+  }
+
+  expect_true(!is.null(app_url))
+
+  app <- AppDriver$new(app_url)
+  withr::defer({ app$stop() })
+
+  expect_error(
+    app$get_values(),
+    "Shiny server returned 404"
+  )
+
+  expect_error(
+    app$expect_values(),
+    "Shiny server returned 404"
+  )
+})
