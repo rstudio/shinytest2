8ae237c3ae85954c8381abab8793d7f83dde6e13
Barret Schloerke <schloerke@gmail.com>
2022-04-06 18:10:26 -0400

    Add setup.R file for wait app

---
+++ tests/testthat/apps/wait/tests/testthat/setup.R
@@ -0,0 +1,14 @@
+if (FALSE) {
+  # TODO-barret; remove this section
+  shinytest2::testing_support() # support for what?
+  shinytest2::load_support()    # conflicting intent with `shiny::loadSupport()`
+  shinytest2::app_support()     # what is support?
+  shinytest2::app_env()         # and do what with it?
+  shinytest2::test_env()        # verb "to test"
+  shinytest2::load_env()        # what env?
+  shinytest2::test_app_env()    # seems like a verb... "to test"
+
+  shinytest2::load_app_env()    # verbose
+}
+
+shinytest2::load_app_env()
