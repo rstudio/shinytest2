928ae0ec22f27b49437625f17fc744db14925730
Barret Schloerke <schloerke@gmail.com>
2022-05-02 14:25:39 -0400

    Test print output

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -1,27 +1,35 @@
 skip_on_cran() # Uses chromote
 
-test_that("wrapper", {
-  expect_equal(1, 1)
-  test_app(test_path("apps/download"))
-  expect_equal(1, 1)
-})
-
+# Test reporter displays info
+# Similar to
+# v |         4 | apps
+# v |         1 | apps - custom name - shinytest2 [0.3s]
+# ✔ |         4 | apps
 test_that("before", {
   expect_equal(1, 1)
   expect_equal(1, 1)
   expect_equal(1, 1)
 })
+test_that("wrapper", {
+  expect_equal(1, 1)
+  test_app(test_path("apps/files-app-rmd"), name = "custom name")
+  expect_equal(1, 1)
+})
+test_that("after", {
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+})
 
+
+## --------------------------------
+
+
+# Test all apps work as expected
 dirs <- Filter(
   dir(test_path("apps"), full.names = TRUE),
   f = function(x) dir.exists(file.path(x, "tests", ""))
 )
 lapply(dirs, function(shiny_app_dir) {
   test_app(shiny_app_dir)
-})
-
-test_that("after", {
-  expect_equal(1, 1)
-  expect_equal(1, 1)
-  expect_equal(1, 1)
 })
