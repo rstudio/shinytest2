d8e4a491d78fc227c376e1a0c19799922b5d6c1d
Barret Schloerke <schloerke@gmail.com>
2022-05-03 10:52:37 -0400

    Merge `test_app()` reporter with exiting test reporter when possible (#192)
    * Temp commit changes to look at other ideas
    
    * Closer! Needs more context in the file being run
    
    * `devtools::document()` (GitHub Actions)
    
    * Put the app name into the testing output. Feels odd that results show up in batches. Need custom reporter?
    
    * Temp commit to not lose everything
    
    * Much nicer output! Need to clean up comments / code
    
    * Clean up code
    
    * Update test-apps.R
    
    * Use devmode for pkgdown so that the dev website does not show up on the real website
    
    * Look into how testthat handles reporter calls internally. Mimic behavior
    
    * Add a test situation of exiting an existing test
    
    * Disable `stop_on_failure` when currently testing by default.
    
    * Test print output
    
    * Make test file for all internal testthat functions / values
    
    * Add docs on how to test multiple apps
    
    * Remove outdated statement
    
    * Add NEWS entry
    
    * Add skip statement to pass tests on CI
    
    * debug CI
    
    * Do not use `.context`... it is deprecated. Instead, use file snapshotter file value
    
    * lint

--- tests/testthat/test-apps.R
+++ tests/testthat/test-apps.R
@@ -1,26 +1,53 @@
 skip_on_cran() # Uses chromote
 
+# TODO-future; Have theses tests run on CI
 
-dirs <- dir(test_path("apps"), full.names = TRUE)
-lapply(dirs, function(shiny_app_dir) {
-  test_path <- file.path(shiny_app_dir, "tests", "")
-  if (dir.exists(test_path)) {
+app_dir <- test_path("apps/files-app-rmd")
+if (!dir.exists(file.path(app_dir, "tests", "testthat"))) {
+  skip("App test folders have been ignored")
+}
 
-    rlang::inform(shiny_app_dir)
-    test_that(paste0("All apps pass their tests - ", shiny_app_dir), {
-      # Test that `test_app()` is performing an expectation in a testing setting
-      expect_success({
-        # Given only testthat tests are used
-        test_app(shiny_app_dir)
-      })
-
-      # # If non-testthat tests are used, this code should be used instead:
-      # expect_error(
-      #   shiny::runTests(shiny_app_dir, assert = TRUE),
-      #   NA,
-      #   info = shiny_app_dir
-      # )
-    })
-
-  }
+# Test reporter displays info
+# Similar to
+# v |         4 | apps
+# v |         1 | apps - custom name - shinytest2 [0.3s]
+# ✔ |         4 | apps
+test_that("before", {
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+  expect_equal(1, 1)
 })
+test_that("wrapper", {
+  expect_equal(1, 1)
+  test_app(app_dir, name = "custom name 1")
+  expect_equal(1, 1)
+})
+test_that("after", {
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+})
+
+test_that("before", {
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+})
+test_app(app_dir, name = "custom name 2")
+test_that("after", {
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+  expect_equal(1, 1)
+})
+
+
+## --------------------------------
+
+
+# Test all apps work as expected
+lapply(
+  dir(test_path("apps"), full.names = TRUE),
+  function(shiny_app_dir) {
+    test_app(shiny_app_dir)
+  }
+)
