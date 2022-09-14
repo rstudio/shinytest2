816cb7c088e978820f6bab123110472fc30e3c7f
Barret Schloerke <schloerke@gmail.com>
2022-09-14 09:04:39 -0400

    Test the two error situations

--- tests/testthat/test-eval-js.R
+++ tests/testthat/test-eval-js.R
@@ -1,25 +1,26 @@
 require("shiny", quietly = TRUE, character.only = TRUE)
 
+shiny_app <- shinyApp(
+  ui = fluidPage(
+    "Eval JS",
+    tags$script("
+      var test_value = 42;
+      var test_string = 'Hello world!';
+    "),
+    verbatimTextOutput("txt")
+  ),
+  server = function(input, output) {
+    # empty
+    output$txt <- renderText({
+      "(app loaded)"
+    })
+  }
+)
+
+
+
 test_that("Duplicate input ids are found", {
-  shiny_app <- shinyApp(
-    ui = fluidPage(
-      "Eval JS",
-      tags$script("
-        var test_value = 42;
-        var test_string = 'Hello world!';
-      "),
-      verbatimTextOutput("txt")
-    ),
-    server = function(input, output) {
-      # empty
-      output$txt <- renderText({
-        "(app loaded)"
-      })
-    }
-  )
-
-
-  app <- AppDriver$new(shiny_app, view = TRUE)
+  app <- AppDriver$new(shiny_app)
   withr::defer(app$stop())
 
   app$wait_for_js("test_value === 42")
@@ -37,4 +38,20 @@ test_that("Duplicate input ids are found", {
   # Provide a test
   # Errors will be thrown above if the conditions can not be met
   expect_true(TRUE)
+})
+
+
+test_that("Errors are caught in $wait_for_js()", {
+  app <- AppDriver$new(shiny_app)
+  withr::defer(app$stop())
+
+  expect_error(
+    app$wait_for_js("test_value === /"),
+    "test_value === /"
+  )
+
+  expect_error(
+    app$wait_for_js("false", timeout = 100),
+    "Timed out waiting for JavaScript script to return `true`"
+  )
 })
