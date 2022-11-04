b626d9ad9e704f95cb7ff8bde5853243d4eb80b8
Barret Schloerke <schloerke@gmail.com>
2022-11-04 10:09:01 -0400

    lint

--- tests/testthat/apps/dir-profile/tests/testthat/test-shinytest2.R
+++ tests/testthat/apps/dir-profile/tests/testthat/test-shinytest2.R
@@ -4,7 +4,7 @@ test_that("{shinytest2} recording: dir-profile", {
   app <- AppDriver$new(name = "dir-profile", height = 1133, width = 1282)
 
   expect_equal(
-    app$get_value(output="txt"),
+    app$get_value(output = "txt"),
     "found!"
   )
 })
