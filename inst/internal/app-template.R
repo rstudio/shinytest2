c55864bb4bb2bea10b0767380990464ccde6d9b4
Barret Schloerke <schloerke@gmail.com>
2023-01-18 20:41:11 -0500

    Use `list2env(globals, envir=globalenv())` to multi assign globals

--- inst/internal/app-template.R
+++ inst/internal/app-template.R
@@ -7,28 +7,10 @@ for (prefix in names(`_data`$resources)) {
   shiny::addResourcePath(prefix, `_data`$resources[[prefix]])
 }
 
-# ## Great for debugging
-# cat("server env (before):\n")
-# print(pryr::rls(environment(`_data`$server)))
+# cat("attaching globals: ", paste0(names(`_data`$globals), collapse = ", "), "\n", sep = "")
 
-# Make globals environment and shim it in between server environment
-# and original server parent environment as to not affect the server environment
-server_parent_env <- parent.env(environment(`_data`$server))
-
-# cat("making globals environment: ", paste0(names(`_data`$globals), collapse = ""), "\n", sep = "")
-app_globals_env <- list2env(`_data`$globals, envir = NULL) # make a new environment
-
-# Shim globals env in between server environment and original server parent environment
-
-# Must be first; Don't want to disconnect the global env if run second
-parent.env(app_globals_env) <- server_parent_env
-# Must be second
-parent.env(environment(`_data`$server)) <- app_globals_env # nolint
-
-# ## Great for debugging
-# cat("globals env:\n")
-# print(pryr::rls(app_globals_env))
-# cat("server env:\n")
-# print(pryr::rls(environment(`_data`$server)))
+# Assign to global environment so that everything is available to each other
+# `list2env(x, envir=globalenv())` is equivalent to multiple calls to `assign(names(x)[[i]], x[[i]], envir = globalenv())`
+list2env(`_data`$globals, envir = globalenv()) # nolint
 
 shinyApp(`_data`$ui, `_data`$server)
