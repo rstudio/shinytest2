27e6803206a0450c785670bcb4218d80e04df34d
Barret Schloerke <schloerke@gmail.com>
2023-01-18 20:39:03 -0500

    Create issue_295.R

---
+++ tests/testthat/scripts/issue_295.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+my_mod_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    textInput(ns("name"), "name", "test"),
+    textOutput(ns("greet"))
+  )
+}
+
+my_mod_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    output$greet <- renderText(input$name)
+  })
+}
+
+make_app2 <- function(mod_ui, mod_server_gen) {
+  force(mod_server_gen)
+  shiny::shinyApp(
+    shiny::fluidPage(mod_ui),
+    function(input, output, session) mod_server_gen()
+  )
+}
+
+app <- make_app2(my_mod_ui("test"), function() { my_mod_server("test") })
+
+driver <- shinytest2::AppDriver$new(app)
+
+# Keep here to print to `stdout`
+print(driver$get_logs())
