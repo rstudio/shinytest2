4ab2862e6422b9e0d185f3bed738cd052a3218c9
Barret Schloerke <schloerke@gmail.com>
2023-02-02 10:09:24 -0800

    Move `profile` app

--- tests/testthat/test-app-rprofile.R
+++ tests/testthat/test-app-rprofile.R
@@ -1,7 +1,12 @@
 library(shinytest2)
 
 test_that("{shinytest2} recording: dir-profile", {
-  app <- AppDriver$new(name = "dir-profile", height = 1133, width = 1282)
+  app <- AppDriver$new(
+    test_path("apps/dir-rprofile"),
+    name = "dir-profile",
+    height = 1133,
+    width = 1282
+  )
 
   expect_equal(
     app$get_value(output = "txt"),
