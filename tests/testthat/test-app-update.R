827ce45b3ab5c2fcfea7d9693bc7631f71030463
Barret Schloerke <schloerke@gmail.com>
2023-02-02 09:30:34 -0800

    Move `update` app

--- tests/testthat/test-app-update.R
+++ tests/testthat/test-app-update.R
@@ -33,4 +33,25 @@ server <- function(input, output, session) {
 
 }
 
-shinyApp(ui, server)
+shiny_app <- shinyApp(ui, server)
+
+
+test_that("click causes input without binding to update", {
+  app <- AppDriver$new(shiny_app, name = "click")
+
+  app$click("click")
+  app$click("click")
+  app$click("click")
+  app$click("click")
+  app$expect_values()
+})
+
+test_that("Can update the input without biding individually", {
+  app <- AppDriver$new(shiny_app, name = "no-binding")
+
+  app$set_inputs(counter = 1, allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(counter = 2, allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(counter = 3, allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(counter = 4, allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$expect_values()
+})
