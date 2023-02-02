b042c0ba2da7c9e37bd925f0dc0bef01976bdc0c
Barret Schloerke <schloerke@gmail.com>
2023-02-02 10:04:02 -0800

    Move `files` apps

--- tests/testthat/apps/files-app-server/tests/testthat/test-shinytest2.R
+++
@@ -1,9 +0,0 @@
-library(shinytest2)
-
-test_that("server.R and app.R are not compatible", {
-  expect_error(
-    AppDriver$new()$stop(),
-    "Unintented behavior may occur",
-    fixed = TRUE
-  )
-})
--- tests/testthat/apps/files-server-ui/tests/testthat/test-shinytest2.R
+++
@@ -1,7 +0,0 @@
-library(shinytest2)
-
-test_that("{shinytest2} recording: kgs", {
-  app <- AppDriver$new(name = "kgs", height = 1321, width = 1221)
-  app$set_inputs(kg = 100)
-  app$expect_values()
-})
