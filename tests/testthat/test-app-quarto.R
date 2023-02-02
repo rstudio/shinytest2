fb3cc8dbb5fe5db36b79f35ec39b8b0aee604d73
Barret Schloerke <schloerke@gmail.com>
2023-02-02 09:49:22 -0800

    Move `quarto` app

--- tests/testthat/test-app-quarto.R
+++ tests/testthat/test-app-quarto.R
@@ -1,11 +1,10 @@
-library(shinytest2)
-
-test_that("Quarto documents can test", {
-  app <- AppDriver$new(seed = 68850)
+test_that("Quarto documents can be tested", {
+  app <- AppDriver$new(test_path("apps/qmd"), seed = 68850)
 
   app$set_inputs(name = "barret")
   app$click("greet")
   app$expect_values()
+
   expect_equal(
     app$get_value(output = "greeting"),
     "Hello barret!"
