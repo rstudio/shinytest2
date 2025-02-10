e89fdd2a868e3bd1ff12bc27394f32753f7f060f
Barret Schloerke <barret@posit.co>
2025-02-10 16:59:18 -0500

    Commit changes before chopping code

--- R/app-driver-start.R
+++ R/app-driver-start.R
@@ -1,7 +1,10 @@
+# Do not delete
+# Paired with tests/testthat/apps/test-env/ 's tests on `internal_shinytest2_value`
 internal_shinytest2_value <- TRUE
 
 app_start_shiny <- function(
-  self, private,
+  self,
+  private,
   ...,
   seed = NULL,
   load_timeout = 10000,
@@ -16,15 +19,39 @@ app_start_shiny <- function(
   # the RNG kind should inherit from the parent process
   rng_kind <- RNGkind()
 
+  # pkgload::pkg_path()
+
+  package_path <- tryCatch(
+    {
+      pkgload::pkg_path()
+    },
+    error = function(e) {
+      NULL
+    }
+  )
+  package_name <- tryCatch(
+    {
+      pkgload::pkg_name()
+    },
+    error = function(e) {
+      NULL
+    }
+  )
+
+  print(list(package_name, package_path))
+
   p <- local({
     # https://github.com/r-lib/testthat/issues/603
     withr::local_envvar(c("R_TESTS" = NA))
     # Load the app's .Rprofile / .Renviron if possible
     withr::local_dir(self$get_dir())
 
+    print("get")
+
     # Find the package name if we're running in a package
-    package_name <- testthat::testing_package()
-    if (!nzchar(package_name)) package_name <- NULL
+    # package_name <- testthat::testing_package()
+    # if (!nzchar(package_name)) package_name <- NULL
+
     # How to load the package is determined by the testthat context
     # * If we're `test_check()`ing, we should load the installed package
     # * Otherwise, we should source the R files before loading the app
@@ -35,69 +62,114 @@ app_start_shiny <- function(
       stderr = sprintf(tempfile_format, "shiny-stderr"),
       supervise = TRUE,
       args = list(
-        app_dir = self$get_dir(),
-        shiny_args = shiny_args,
-        has_rmd = app_dir_has_rmd(self, private),
-        seed = seed,
-        rng_kind = rng_kind,
-        render_args = render_args,
-        options = options,
-        package_name = package_name,
-        load_package = load_package
+        .app_dir = self$get_dir(),
+        .shiny_args = shiny_args,
+        .has_rmd = app_dir_has_rmd(self, private),
+        .seed = seed,
+        .rng_kind = rng_kind,
+        .render_args = render_args,
+        .options = options,
+        .package_name = package_name,
+        .package_path = package_path,
+        .load_package = load_package
       ),
       function(
-        app_dir,
-        shiny_args,
-        has_rmd,
-        seed,
-        rng_kind,
-        render_args,
-        options,
-        package_name,
-        load_package
+        .app_dir,
+        .shiny_args,
+        .has_rmd,
+        .seed,
+        .rng_kind,
+        .render_args,
+        .options,
+        .package_name,
+        .package_path,
+        .load_package
       ) {
-
-        if (!is.null(seed)) {
+        if (!is.null(.seed)) {
           # Prior to R 3.6, RNGkind has 2 args, otherwise it has 3
-          do.call(RNGkind, as.list(rng_kind))
-          set.seed(seed)
-          getNamespace("shiny")$withPrivateSeed(set.seed(seed + 11))
+          do.call(RNGkind, as.list(.rng_kind))
+          set.seed(.seed)
+          getNamespace("shiny")$withPrivateSeed(set.seed(.seed + 11))
         }
 
-        options <- as.list(options)
-        options[["shiny.testmode"]] <- TRUE
+        .options <- as.list(.options)
+        .options[["shiny.testmode"]] <- TRUE
         # TODO-future; Adjust shiny to add htmldeps to the list of the rendered page
         # options[["shiny-testmode-html-dep"]] <- getTracerDep()
-        do.call(base::options, options)
+        do.call(base::options, .options)
 
-        # Add testthat to app's environment
-        library(testthat)
-        testthat___test_files_setup_env <- getFromNamespace("test_files_setup_env", "testthat")
+        # Do not use testthat:::test_files_setup as it will load the library when `load_package == "installed"`
+        # start: Alteration of testthat:::test_files_setup where {testthat} and the package are not loaded
+        # Alteration comments done in `##` comments
 
-        test_env = testthat___test_files_setup_env(
-          test_package = package_name,
-          test_dir = app_dir,
-          load_package = load_package,
-          env = NULL
-        )
-        withr::local_environment(test_env)
+        ## Suppress testthat from loading
+        # library(testthat)
+
+        ## No need to arg match static values
+        # load_package <- rlang::arg_match(load_package, c("none", "installed", "source"))
+        if (.load_package == "installed") {
+          ## Suppress package from loading.
+          ## Require the user to ask for it in their app!
+          # library(test_package, character.only = TRUE)
+        } else if (.load_package == "source") {
+          ## Use `asNamespace("testthat")` to access non-exported methods
+          # Allow configuring what we export to the search path (#1636)
+          # desc$get_field("Config/testthat/load-all", default = NULL)
+          # args <- asNamespace("testthat")$find_load_all_args(test_dir)
+
+          pkgload::load_all(
+            ## This feels wrong. Feels like it should be the pkg's dir
+            .package_path,
+            #.test_dir,
+            ## Do not attach package. Make the user call `library(pkg)` themselves
+            # attach = FALSE,
+
+            # ## Be sure to attach the local package to the search path
+            attach = TRUE,
+
+            # ## Do not export any values
+            export_all = FALSE,
+            # helpers = args[["helpers"]],
+            quiet = TRUE
+          )
+        }
+
+        # .test_env <- testthat::test_env(.package_name)
+
+        # end: altered testthat:::test_files_setup
+
+        # # Add testthat to app's environment
+        # library(testthat)
+        # .testthat___test_files_setup_env <- getFromNamespace(
+        #   "test_files_setup_env",
+        #   "testthat"
+        # )
+
+        # .test_env = .testthat___test_files_setup_env(
+        #   test_package = .package_name,
+        #   test_dir = .app_dir,
+        #   load_package = .load_package,
+        #   env = NULL
+        # )
+
+        # withr::local_environment(.test_env)
 
         # Return value is important for `AppDriver$stop()`
         # Do not add code after this if else block
-        if (has_rmd) {
+        if (.has_rmd) {
           # Shiny document
           rmarkdown::run(
             file = NULL, # Do not open anything in browser
-            dir = app_dir,
+            dir = .app_dir,
             default_file = NULL, # Let rmarkdown find the default file
             # DO NOT ENABLE! Makes things like `app$wait_for_idle()` not work as expected.
             auto_reload = FALSE, # Do not constantly poll for file changes. Drastically reduces `app$get_logs()`
-            shiny_args = shiny_args,
-            render_args = render_args
+            shiny_args = .shiny_args,
+            render_args = .render_args
           )
         } else {
           # Normal shiny app
-          do.call(shiny::runApp, c(app_dir, shiny_args))
+          do.call(shiny::runApp, c(.app_dir, .shiny_args))
         }
       }
     )
@@ -106,11 +178,15 @@ app_start_shiny <- function(
   private$shiny_process <- p # nolint
 
   "!DEBUG waiting for shiny to start"
-  if (! p$is_alive()) {
-    app_abort(self, private, paste0(
-      "Failed to start shiny. Error:\n",
-      strwrap(readLines(p$get_error_file()))
-    ))
+  if (!p$is_alive()) {
+    app_abort(
+      self,
+      private,
+      paste0(
+        "Failed to start shiny. Error:\n",
+        strwrap(readLines(p$get_error_file()))
+      )
+    )
   }
 
   "!DEBUG finding shiny port"
@@ -121,27 +197,35 @@ app_start_shiny <- function(
     err_lines <- readLines(p$get_error_file())
 
     if (!p$is_alive()) {
-      app_abort(self, private, paste0(
-        "Error starting shiny application:\n",
-        paste(err_lines, collapse = "\n")
-      ))
+      app_abort(
+        self,
+        private,
+        paste0(
+          "Error starting shiny application:\n",
+          paste(err_lines, collapse = "\n")
+        )
+      )
     }
     if (any(grepl("Listening on http", err_lines, fixed = TRUE))) break
 
     Sys.sleep(0.2)
 
     if (i == max_i) {
-      app_abort(self, private, sprintf(
-        paste(
-          "The Shiny app failed to start up within %s second%s.",
-          "To increase the loading timeout, consult the documentation in `?AppDriver`",
-          "for the `load_timeout` argument of `AppDriver$new()`.",
-          "The app printed the following lines to stderr during start up:\n%s"
-        ),
-        load_timeout / 1000,
-        if (load_timeout == 1000) "" else "s",
-        paste(err_lines, collapse = "\n")
-      ))
+      app_abort(
+        self,
+        private,
+        sprintf(
+          paste(
+            "The Shiny app failed to start up within %s second%s.",
+            "To increase the loading timeout, consult the documentation in `?AppDriver`",
+            "for the `load_timeout` argument of `AppDriver$new()`.",
+            "The app printed the following lines to stderr during start up:\n%s"
+          ),
+          load_timeout / 1000,
+          if (load_timeout == 1000) "" else "s",
+          paste(err_lines, collapse = "\n")
+        )
+      )
     }
   }
 
