aef822fcf4075ded63e2dbdd5c5d43aa5a9dbc27
Barret Schloerke <barret@posit.co>
2025-11-21 07:49:51 -0500

    slow down test with extra wait

--- tests/testthat/test-app-update.R
+++ tests/testthat/test-app-update.R
@@ -3,22 +3,26 @@ library(shiny)
 ui <- fluidPage(
   tags$h1("Do not touch slider"),
   actionButton("click", "Click me!"),
-  tagAppendAttributes(sliderInput("slider", "Slider", 0, 100, value = 50), class = "disabled"),
+  tagAppendAttributes(
+    sliderInput("slider", "Slider", 0, 100, value = 50),
+    class = "disabled"
+  ),
   tags$h4("Click count"),
   verbatimTextOutput("clicks"),
   tags$h4("JS Click count"),
   verbatimTextOutput("js_clicks"),
-  tags$script(HTML('
+  tags$script(HTML(
+    '
     $(function() {
       Shiny.addCustomMessageHandler("count", function(message) {
         Shiny.setInputValue("counter", message, {priority: "event"});
       });
     });
-  '))
+  '
+  ))
 )
 
 server <- function(input, output, session) {
-
   observe({
     shiny::updateSliderInput(inputId = "slider", value = input$click)
     session$sendCustomMessage("count", input$click)
@@ -30,7 +34,6 @@ server <- function(input, output, session) {
   output$js_clicks <- renderText({
     input$counter
   })
-
 }
 
 shiny_app <- shinyApp(ui, server)
@@ -43,15 +46,34 @@ test_that("click causes input without binding to update", {
   app$click("click")
   app$click("click")
   app$click("click")
+
+  app$wait_for_idle()
+
   app$expect_values()
 })
 
 test_that("Can update the input without biding individually", {
   app <- AppDriver$new(shiny_app, name = "no-binding")
 
-  app$set_inputs(counter = 1, allow_no_input_binding_ = TRUE, priority_ = "event")
-  app$set_inputs(counter = 2, allow_no_input_binding_ = TRUE, priority_ = "event")
-  app$set_inputs(counter = 3, allow_no_input_binding_ = TRUE, priority_ = "event")
-  app$set_inputs(counter = 4, allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(
+    counter = 1,
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$set_inputs(
+    counter = 2,
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$set_inputs(
+    counter = 3,
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$set_inputs(
+    counter = 4,
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
   app$expect_values()
 })
